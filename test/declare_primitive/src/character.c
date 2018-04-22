db7396efcab03f72294de10df94abe337728e348
Sander Mertens <sander.mertens8@gmail.com>
2018-04-22 11:27:25 -0700

    Add character testcases

--- test/declare_primitive/src/character.c
+++ test/declare_primitive/src/character.c
@@ -2,38 +2,134 @@
 
 #include <include/test.h>
 
-
-
 void test_character_tc_alpha(
     test_character this)
 {
-    /* Insert implementation */
-}
+    const char *input = "char obj: 'a'";
+    ast_Node ast = cortoscript_ast_parse(input);
+    test_assert(ast != NULL);
 
+    int16_t ret = cortoscript_ast_declare(data_o, ast);
+    test_assert(ret == 0);
+
+    char *obj = corto_lookup(data_o, "obj");
+    test_assert(obj != NULL);
+    test_assert(corto_typeof(obj) == (corto_type)corto_char_o);
+    test_assert(corto_check_state(obj, CORTO_VALID));
+    test_assert(corto_countof(obj) == 2);
+    test_assert(*obj == 'a');
+
+    test_assert(corto_delete(ast) == 0);
+    test_assert(corto_release(obj) == 1);
+    test_assert(corto_delete(obj) == 0);
+}
 
 void test_character_tc_digit(
     test_character this)
 {
-    /* Insert implementation */
-}
+    const char *input = "char obj: '3'";
+    ast_Node ast = cortoscript_ast_parse(input);
+    test_assert(ast != NULL);
 
+    int16_t ret = cortoscript_ast_declare(data_o, ast);
+    test_assert(ret == 0);
+
+    char *obj = corto_lookup(data_o, "obj");
+    test_assert(obj != NULL);
+    test_assert(corto_typeof(obj) == (corto_type)corto_char_o);
+    test_assert(corto_check_state(obj, CORTO_VALID));
+    test_assert(corto_countof(obj) == 2);
+    test_assert(*obj == '3');
+
+    test_assert(corto_delete(ast) == 0);
+    test_assert(corto_release(obj) == 1);
+    test_assert(corto_delete(obj) == 0);
+}
 
 void test_character_tc_newline(
     test_character this)
 {
-    /* Insert implementation */
+    const char *input = "char obj: '\\n'";
+    ast_Node ast = cortoscript_ast_parse(input);
+    test_assert(ast != NULL);
+
+    int16_t ret = cortoscript_ast_declare(data_o, ast);
+    test_assert(ret == 0);
+
+    char *obj = corto_lookup(data_o, "obj");
+    test_assert(obj != NULL);
+    test_assert(corto_typeof(obj) == (corto_type)corto_char_o);
+    test_assert(corto_check_state(obj, CORTO_VALID));
+    test_assert(corto_countof(obj) == 2);
+    test_assert(*obj == '\n');
+
+    test_assert(corto_delete(ast) == 0);
+    test_assert(corto_release(obj) == 1);
+    test_assert(corto_delete(obj) == 0);
 }
 
-
-void test_character_tc_null(
+void test_character_tc_null_character(
     test_character this)
 {
-    /* Insert implementation */
-}
+    const char *input = "char obj: '\\0'";
+    ast_Node ast = cortoscript_ast_parse(input);
+    test_assert(ast != NULL);
 
+    int16_t ret = cortoscript_ast_declare(data_o, ast);
+    test_assert(ret == 0);
+
+    char *obj = corto_lookup(data_o, "obj");
+    test_assert(obj != NULL);
+    test_assert(corto_typeof(obj) == (corto_type)corto_char_o);
+    test_assert(corto_check_state(obj, CORTO_VALID));
+    test_assert(corto_countof(obj) == 2);
+    test_assert(*obj == '\0');
+
+    test_assert(corto_delete(ast) == 0);
+    test_assert(corto_release(obj) == 1);
+    test_assert(corto_delete(obj) == 0);
+}
 
 void test_character_tc_number(
     test_character this)
 {
-    /* Insert implementation */
+    const char *input = "char obj: 65";
+    ast_Node ast = cortoscript_ast_parse(input);
+    test_assert(ast != NULL);
+
+    int16_t ret = cortoscript_ast_declare(data_o, ast);
+    test_assert(ret == 0);
+
+    char *obj = corto_lookup(data_o, "obj");
+    test_assert(obj != NULL);
+    test_assert(corto_typeof(obj) == (corto_type)corto_char_o);
+    test_assert(corto_check_state(obj, CORTO_VALID));
+    test_assert(corto_countof(obj) == 2);
+    test_assert(*obj == 'A');
+
+    test_assert(corto_delete(ast) == 0);
+    test_assert(corto_release(obj) == 1);
+    test_assert(corto_delete(obj) == 0);
+}
+
+void test_character_tc_zero(
+    test_character this)
+{
+    const char *input = "char obj: 0";
+    ast_Node ast = cortoscript_ast_parse(input);
+    test_assert(ast != NULL);
+
+    int16_t ret = cortoscript_ast_declare(data_o, ast);
+    test_assert(ret == 0);
+
+    char *obj = corto_lookup(data_o, "obj");
+    test_assert(obj != NULL);
+    test_assert(corto_typeof(obj) == (corto_type)corto_char_o);
+    test_assert(corto_check_state(obj, CORTO_VALID));
+    test_assert(corto_countof(obj) == 2);
+    test_assert(*obj == '\0');
+
+    test_assert(corto_delete(ast) == 0);
+    test_assert(corto_release(obj) == 1);
+    test_assert(corto_delete(obj) == 0);
 }
